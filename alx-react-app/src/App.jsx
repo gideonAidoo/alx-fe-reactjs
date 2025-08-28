715be01ff810b9e6d0ab315069a0b5b6d000cc7d
Gideon Aidoo <gideonaidoo2016@gmail.com>
2025-08-28 15:56:36 +0000

    adding props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,10 +6,12 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
 
 
 function App() {
   const [count, setCount] = useState(0)
+  
 
   return (
     <>
@@ -18,7 +20,15 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
-      
+
+
+        <UserProfile 
+          name="Alice" 
+          age="25" 
+          bio="Loves hiking and photography" 
+        />
+
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
