a2d6ec814c6ff2534e4419bb9aa1e1ffc746af7d
Gideon Aidoo <gideonaidoo2016@gmail.com>
2025-08-28 15:37:15 +0000

    added three components mainContent, Header, and footer

---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Maincontent() {
+    return (
+
+        <main>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+
+    );
+}
+
+export default Maincontent;
