0118841d2c07fa55d2fa6bdb28d5494f26a114de
Gideon Aidoo <gideonaidoo2016@gmail.com>
2025-09-08 18:20:21 +0000

    added Counter component to my react project

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -6,20 +6,19 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'
 import UserProfile from './components/UserProfile'
-
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
   
-
   return (
     <>
       <div>
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <Counter />
       <Footer />
-
 
         <UserProfile 
           name="Alice" 
