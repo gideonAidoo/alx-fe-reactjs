4a69cc4ac7094293330f2f03c0c8154cc0d09f03
Gideon Aidoo <gideonaidoo2016@gmail.com>
2025-09-13 15:49:46 +0000

    adding functionallity

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,18 +1,21 @@
 // src/components/RecipeDetails.jsx
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { useRecipeStore } from './recipeStore';
-
+import { useRecipeStore } from './recipeStore'; 
 const RecipeDetails = () => {
   const { id } = useParams();
 
-  // find recipe by id from the store
   const recipe = useRecipeStore((state) =>
     state.recipes.find((r) => r.id === Number(id))
   );
 
   if (!recipe) {
-    return <p>Recipe not found!</p>;
+    return (
+      <div>
+        <p>Recipe not found!</p>
+        <Link to="/">Back to Recipes</Link>
+      </div>
+    );
   }
 
   return (
@@ -20,11 +23,9 @@ const RecipeDetails = () => {
       <h2>{recipe.title}</h2>
       <p>{recipe.description}</p>
 
-      {/* notice we use recipe.id safely here */}
       <Link to={`/edit/${recipe.id}`} style={{ marginRight: '10px' }}>
         Edit Recipe
       </Link>
-
       <Link to="/">Back to Recipes</Link>
     </div>
   );
