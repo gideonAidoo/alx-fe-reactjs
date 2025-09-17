323f73664c7a25426dd45c0b97240f20eadf3275
Gideon Aidoo <gideonaidoo2016@gmail.com>
2025-09-17 19:29:00 +0000

    Implementing search button

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,14 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/users/";
+const api = axios.create({
+  baseURL: "https://api.github.com",
+});
 
-// Fetch user data by username
 export const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(`${BASE_URL}${username}`);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching user data:", error);
-    return null;
-  }
+  const response = await api.get(`/users/${username}`);
+  return response.data;
 };
