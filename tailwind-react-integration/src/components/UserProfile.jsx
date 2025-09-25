5fcc0d68b64bb5a9343d8a89fca9227a8faefa14
Gideon Aidoo <gideonaidoo2016@gmail.com>
2025-09-25 12:10:07 +0000

    adding responsiveness to my project

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,17 @@
-
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 md:p-8 sm:p-4 max-w-xm mx-auto my-20 rounded-lg shadow-lg">
-      <img className="rounded-full md:w-36 md:h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
-      <h1 className="sm:text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+      <img
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto object-cover"
+        src="https://via.placeholder.com/150"
+        alt="User"
+      />
+      <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4 font-bold">
+        John Doe
+      </h1>
+      <p className="text-sm sm:text-sm md:text-base text-gray-600">
+        Developer at Example Co. Loves to write code and explore new technologies.
+      </p>
     </div>
   );
 }
