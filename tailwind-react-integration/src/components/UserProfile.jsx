0b26614a17e7b8112b023c9f0a760a898a2ecc50
Gideon Aidoo <gideonaidoo2016@gmail.com>
2025-09-25 12:20:19 +0000

    adding responsiveness to my project

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 md:max-w-sm max-w-xs mx-auto my-20 rounded-lg shadow-lg text-center">
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 md:max-w-sm max-w-xs mx-auto my-20 rounded-lg shadow-lg text-center hover:shadow-xl ">
       <img
-        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto object-cover"
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto object-cover hover:scale-110 transition-transform duration-300 ease-in-out
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4 font-bold">
+      <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4 font-bold hover:text-blue-500">
         John Doe
       </h1>
       <p className="text-sm sm:text-sm md:text-base text-gray-600">
