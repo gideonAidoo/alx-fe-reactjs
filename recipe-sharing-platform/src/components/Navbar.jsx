b1d975717a9bc0ef53e8cda54d83b12c57162ee6
Gideon Aidoo <gideonaidoo2016@gmail.com>
2025-09-30 22:42:24 +0000

    setting up the project and implementing tailwind css

---
+++ recipe-sharing-platform/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+<div className=" bg-amber-600 p-4 border-b-2 border-amber-400">
+    <h1 className="text-3xl font-bold text-amber-50 ">Navbar</h1>
+</div> 
+ )
+}
+
+export default Navbar;
