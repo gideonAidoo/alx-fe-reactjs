0f2c04de6d258476d29fb3d431d4009a6c2694d9
Gideon Aidoo <gideonaidoo2016@gmail.com>
2025-10-02 09:47:36 +0000

    creats a responsive Home Page that presents a list of recipes in an attractive and user-friend ly layout and json file

--- recipe-sharing-platform/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-function Navbar() {
-  return (
-<div className=" bg-amber-600 p-4 border-b-2 border-purple-400">
-    <h1 className="text-3xl font-bold text-amber-50 ">Navbar</h1>
-</div> 
- )
-}
-
-export default Navbar;
